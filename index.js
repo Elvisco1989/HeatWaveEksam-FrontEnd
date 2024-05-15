a3825ad266357f14bd0bf960d29ae1787a92f67b
Mbah Elvis Ndum <130441973+Elvisco1989@users.noreply.github.com>
2024-05-15 12:52:03 +0200

    Adding more functions

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 
+
 const url = 'https://heatwaveprojekt.azurewebsites.net/api/temp';
 
 Vue.createApp({
@@ -7,11 +8,10 @@ Vue.createApp({
             Measurements: [],
             error: "",
             addMessage: "",
-                AddMeasurement: {
+            AddMeasurement: {
                 IndoorTemperature: 0,
                 OutDoorTemperature: 0,
                 Date: 0,
-                
             },
             updateMessage: "",
             updateData: {
@@ -19,40 +19,78 @@ Vue.createApp({
                 IndoorTemperature: 0,
                 OutDoorTemperature: 0,
                 Date: 0,
-            }
-        };
-    },
-    created() {
-        this.getAll();
-    },
-    methods: {
-        async getAll() {
-            try {
-                const response = await axios.get(url);
-                this.Measurements = response.data;
-            } catch (error) {
-                this.error = error.message;
-            }
+            },
+            currentMeasurement: null,// Initialize to null
+            recommendedTemp: true,
+            
+            };
         },
-        async addMeasurement() {
-            try {
-                response = await axios.post(url, this.AddMeasurement);
-                this.addMessage = "response " + response.status + " " + response.statusText;
-                this.getAll();
-            } catch (ex) {
-                alert(ex.message);
-            }
-        },
-        async updateMeasurement() {
-            const url = url + "/" + this.updateData.id
-            try {
-                response = await axios.put(url, this.updateData)
-                this.updateMessage = "response " + response.status + " " + response.statusText
-                this.getAll()
-            } catch (ex) {
-                alert(ex.message)
-            }
-        }
+        computed: {
+            temperatureDifference: function () {
+                return this.currentMeasurement.inDoorTemperature - this.currentMeasurement.outDoorTemperature;
 
-    }
-}).mount("#App");
+            },
+        }, // Add a comma here
+        created() {
+            this.getAll();
+            this.getCurrentMeasurementWithMaxId(); // Call the method to get the current measurement with the maximum ID
+        },
+        methods: {
+        async getAll() {
+                try {
+                    const response = await axios.get(url);
+                    this.Measurements = response.data;
+                } catch (error) {
+                    this.error = error.message;
+                }
+            },
+        async addMeasurement() {
+                try {
+                    const response = await axios.post(url, this.AddMeasurement);
+                    this.addMessage = "response " + response.status + " " + response.statusText;
+                    this.getAll();
+                } catch (ex) {
+                    alert(ex.message);
+                }
+            },
+        async updateMeasurement() {
+                const updateUrl = url + "/" + this.updateData.id; // Changed variable name to avoid conflict
+                try {
+                    const response = await axios.put(updateUrl, this.updateData); // Added 'const' to define the response variable
+                    this.updateMessage = "response " + response.status + " " + response.statusText;
+                    this.getAll();
+                } catch (ex) {
+                    alert(ex.message);
+                }
+            },
+            getCurrentMeasurementWithMaxId() {
+                // const Measurements = this.getAll();
+                if (this.Measurements.length > 0) {
+                    // Get the last item in the array
+                    const lastMeasurement = this.Measurements[this.Measurements.length - 1];
+
+                    // Set the currentMeasurement to the last measurement
+                    this.currentMeasurement = lastMeasurement;
+                }
+            },
+            // temperatureDifference() {
+            //     // Get the current measurement with the maximum ID
+            //     this.getCurrentMeasurementWithMaxId();
+
+            //     // Check if the currentMeasurement is not null
+            //     if (this.currentMeasurement != null) {
+            //         // Calculate the temperature difference
+            //         temperatureDifference = this.currentMeasurement.IndoorTemperature - this.currentMeasurement.OutDoorTemperature;
+            //         // Return the temperature difference
+            //         return temperatureDifference;
+            //     } else { // If the currentMeasurement is null
+            //         // Return a message indicating no measurements available
+            //         System.out.println("No measurements available");
+            //         return 0; // or you can return NaN (Not a Number) to indicate invalid result
+            //     }
+            // }
+
+
+        }
+    }).mount("#App");
+
